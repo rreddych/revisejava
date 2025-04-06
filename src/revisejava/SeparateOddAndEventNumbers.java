2b13e3c568087113c4c8ba0a94a06ae1397fae61
Rajesh Chinta <rajeshreddychinta@gmail.com>
2025-04-06 12:30:00 +0530

    using partitioningBy separate odd and event numbers in a list

--- src/revisejava/SeparateOddAndEventNumbers.java
+++ src/revisejava/SeparateOddAndEventNumbers.java
@@ -2,12 +2,22 @@ package revisejava;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class SeparateOddAndEventNumbers {
 
 	public static void main(String[] args) {
 		List<Integer> listOfIntegers = Arrays.asList(71, 18, 42, 21, 67, 32, 95, 14, 56, 87);
-		listOfIntegers.stream().filter(i -> i%2 ==0).forEach(i -> System.out.println(i));
-		listOfIntegers.stream().filter(i -> i%2 !=0).forEach(i -> System.out.println(i));
+		Map<Boolean, List<Integer>> seperatedMap = listOfIntegers.stream()
+				.collect(Collectors.partitioningBy(i -> i % 2 == 0));
+		System.out.println("Even Numbers: ");
+		seperatedMap.get(true).forEach(i -> {
+			System.out.print(i + " ");
+		});
+		System.out.println("\nOdd Numbers: ");
+		seperatedMap.get(false).forEach(i -> {
+			System.out.print(i + " ");
+		});
 	}
 }
