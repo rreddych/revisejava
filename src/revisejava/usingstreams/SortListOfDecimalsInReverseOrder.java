7ffc69057479a39940f8de2519e9e49d8e65ee53
Rajesh Chinta <rajeshreddychinta@gmail.com>
2025-04-07 09:34:05 +0530

    SortListOfDecimalsInReverseOrder

---
+++ src/revisejava/usingstreams/SortListOfDecimalsInReverseOrder.java
@@ -0,0 +1,17 @@
+package revisejava.usingstreams;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class SortListOfDecimalsInReverseOrder {
+
+	public static void main(String[] args) {
+		List<Double> doubleList = Arrays.asList(23.0, 56.7, 89.3,12.4, 52.6, 22.5);
+		// sorted in natural (ascending) order.
+		doubleList.stream().sorted().forEach(System.out::println);
+		// sort in reverse (descending) order 
+		System.out.println("Sorted in reverse order: ");
+		doubleList.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println);
+	}
+}
