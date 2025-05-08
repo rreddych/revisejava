926852d059410005a1dda3cd6c4b071b3c73739f
Rajesh Chinta <rajeshreddychinta@gmail.com>
2025-05-08 18:34:22 +0530

    Converted the project to maven

---
+++ src/test/java/com/learn/others/NoOfPairsSummingToResultTest.java
@@ -0,0 +1,14 @@
+package com.learn.others;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class NoOfPairsSummingToResultTest {
+
+	@Test
+	void test() {
+		//fail("Not yet implemented");
+	}
+
+}
