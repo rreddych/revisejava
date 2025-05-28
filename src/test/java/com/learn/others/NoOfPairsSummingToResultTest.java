ee9c04927df711780e6db17f908bc9e06ef8c276
Rajesh Chinta <rajeshreddychinta@gmail.com>
2025-05-28 21:46:38 +0530

    Added tests to Num of pairs summing up to result

--- src/test/java/com/learn/others/NoOfPairsSummingToResultTest.java
+++ src/test/java/com/learn/others/NoOfPairsSummingToResultTest.java
@@ -7,8 +7,15 @@ import org.junit.jupiter.api.Test;
 class NoOfPairsSummingToResultTest {
 
 	@Test
-	void test() {
-		//fail("Not yet implemented");
+	void testNumOfPairsSummingToResult() {
+		int numOfPairs = 0;
+		NoOfPairsSummingToResult noOfPairsSummingToResult = new NoOfPairsSummingToResult();
+		numOfPairs = noOfPairsSummingToResult.getNoOfPairsSummingToResult(new int[] {1,5,7,3,6,2,9,-1}, 8);
+		assertEquals(4, numOfPairs);
+		
+		numOfPairs = noOfPairsSummingToResult.getNoOfPairsSummingToResult(new int[] {1,5,7,-1}, 6);
+		assertEquals(2, numOfPairs);
 	}
+
 
 }
